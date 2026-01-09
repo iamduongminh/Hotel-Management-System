996ac3e8b812de34c0476222b9d2d580ac5f9d54
Minh Duong <contact.minhdq@gmail.com>
2026-01-09 21:32:28 +0700

    fix: SessionManager can't be Singleton

--- src/main/java/com/hotel_management/infrastructure/SessionManager.java
+++ src/main/java/com/hotel_management/infrastructure/SessionManager.java
@@ -1,22 +1,28 @@
 package com.hotel_management.infrastructure;
 
 import org.springframework.stereotype.Component;
-
 import com.hotel_management.api.core.domain.entity.User;
+import jakarta.servlet.http.HttpSession;
 
 @Component
 public class SessionManager {
-    private User currentUser;
+
+    private final HttpSession httpSession;
+
+    public SessionManager(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
 
     public void setCurrentUser(User user) {
-        this.currentUser = user;
+        // Lưu vào bộ nhớ Session của trình duyệt cụ thể
+        httpSession.setAttribute("CURRENT_USER", user);
     }
 
     public User getCurrentUser() {
-        return currentUser;
+        return (User) httpSession.getAttribute("CURRENT_USER");
     }
 
     public void logout() {
-        this.currentUser = null;
+        httpSession.removeAttribute("CURRENT_USER");
     }
 }
